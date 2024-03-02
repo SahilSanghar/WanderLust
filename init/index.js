b99f9c45e10fecf429576d6d7c337728860b995d
SAHIL sanghar <sangharsahil546@gmail.com>
2024-03-02 14:54:18 +0530

    Inserted aditional info of owner in existing data

--- init/index.js
+++ init/index.js
@@ -18,6 +18,10 @@ async function main() {
 
 const initDB = async () => {
   await Listing.deleteMany({});
+  initData.data = initData.data.map((obj) => ({
+    ...obj,
+    owner: "65e052129d3e2a0f13f564c0",
+  }))
   await Listing.insertMany(initData.data);
   console.log("data was initialized");
 };
