483fda537f1df22940a4b8d7e82f63a1c3cdf3e4
SAHIL sanghar <sangharsahil546@gmail.com>
2024-03-02 15:08:25 +0530

    Added sessions, cookies, flash, passport and routers

--- app.js
+++ app.js
@@ -5,11 +5,26 @@ const path = require("path")
 const methodOverride = require("method-override")
 const ejsMate = require("ejs-mate")
 const ExpressError = require("./utils/ExpressError.js")
-
-const listings = require("./routes/listing.js")
-const reviews = require("./routes/review.js")
+const session = require("express-session")
+const flash = require("connect-flash")
+const listingRouter = require("./routes/listing.js")
+const reviewRouter = require("./routes/review.js")
+const userRouter = require("./routes/user.js")
+const passport = require("passport")
+const LocalStrategy = require("passport-local")
+const User = require("./models/user.js")
 
 const mongo_url = "mongodb://127.0.0.1:27017/WanderLust";
+const sessionOption = { 
+    secret: "mysupersecretstring",
+    resave: false,
+    saveUninitialized: true,
+    cookie: {
+        expire: Date.now() + 7 * 24 * 60 * 1000,
+        maxAge: 7 * 24 * 60 * 1000,
+        httpOnly: true,
+    }
+};
 
 app.set("view engine", "ejs");
 app.set("views", path.join(__dirname, "views/listings"))
@@ -18,6 +33,16 @@ app.use(methodOverride("_method"))
 app.engine("ejs", ejsMate)
 app.use(express.static(path.join(__dirname, "/public")))
 
+app.use(session(sessionOption))
+app.use(flash())
+
+app.use(passport.initialize())
+app.use(passport.session())
+passport.use(new LocalStrategy(User.authenticate()))
+
+passport.serializeUser(User.serializeUser())
+passport.deserializeUser(User.deserializeUser())
+
 main()
 .then(()=> {
     console.log("Connected to DB")
@@ -25,22 +50,32 @@ main()
 .catch((err)=>{
     console.log(err)
 })
+
 async function main(){
     await mongoose.connect(mongo_url)
 }
 
-//Testing route
-app.get("/",(req, res)=> {
-    res.send("Hi, I am root")
+app.use((req, res, next) => {
+    res.locals.success = req.flash("success");
+    res.locals.error = req.flash("error");
+    res.locals.currUser = req.user;
+    next();
 })
+
+// app.get("/demouser", async(req, res) => {
+//     let fakeUser = new User({
+//         email: "student@gmail.com",
+//         username: "delta-student"
+//     })
+
+//     let registerUser = await User.register(fakeUser, "helloworld")
+//     res.send(registerUser)
+// })
 
 //Router
-app.use("/listings", listings)
-app.use("listings/:id/reviews", reviews)
-
-app.listen(8080, ()=> {
-    console.log("App is listening to port 8080")
-})
+app.use("/listings", listingRouter)
+app.use("/listings/:id/reviews", reviewRouter)
+app.use("/", userRouter)
 
 //Error handler
 
@@ -52,4 +87,8 @@ app.use((err, req, res, next) => {
     let { statusCode= 500, message= "Something went wrong!" } = err;
     res.status(statusCode).render("error.ejs", {message})
     // res.status(statusCode).send(message)
+})
+
+app.listen(8080, ()=> {
+    console.log("App is listening to port 8080")
 })
