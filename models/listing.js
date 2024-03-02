c5b4d57a9de1b04f49444d4b97135dd0281ee020
SAHIL sanghar <sangharsahil546@gmail.com>
2024-03-02 14:52:18 +0530

    Added owner in the schema

--- models/listing.js
+++ models/listing.js
@@ -13,9 +13,8 @@ const listingSchema = new Schema({
     image: {
         filename: String,
         url: String,
-        // default: "https://varnitec.com/sites/default/files/2020-06/2.jpg",
-        // set: (v) => v === ""? "https://varnitec.com/sites/default/files/2020-06/2.jpg": v,
-
+        // default: String,
+        // Set: (v) => v === ""? "https://images.unsplash.com/photo-1488462237308-ecaa28b729d7?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8OXx8c2t5JTIwdmFjYXRpb258ZW58MHx8MHx8fDA%3D&auto=format&fit=crop&w=800&q=60": v,
     },
     price: {
         type: Number,
@@ -31,7 +30,11 @@ const listingSchema = new Schema({
             type: Schema.Types.ObjectId,
             ref: "Review",
         },
-    ]
+    ],
+    owner: {
+            type: Schema.Types.ObjectId,
+            ref: "User",
+        }
 }); 
 
 listingSchema.post("findOneAndDelete", async(listing) => {
