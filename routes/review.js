0fbf1bcc383fc0587c59c88c287452ce90abdbcc
SAHIL sanghar <sangharsahil546@gmail.com>
2024-03-02 14:58:20 +0530

    Added flash messages for creating and deleting reviews

--- routes/review.js
+++ routes/review.js
@@ -27,7 +27,7 @@ router.post("/", validateReview, wrapAsync(async(req, res) => {
     
     await newReview.save()
     await listing.save()
-
+    req.flash("success", "New review created!")
     res.redirect(`/listings/${listing._id}`)
 }))
 
@@ -37,7 +37,7 @@ router.delete("/:reviewId", wrapAsync(async(req, res)=> {
 
     await Listing.findByIdAndUpdate(id, {$pull: {reviews: reviewId}})
     await Review.findByIdAndDelete(reviewId)
-
+    req.flash("success", "Review deleted!")
     res.redirect(`/listings/${id}`)
 }))
 
