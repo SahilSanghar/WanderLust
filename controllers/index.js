ad422de3462c1b85d945dabccf93014018f0d19a
SAHIL sanghar <sangharsahil546@gmail.com>
2024-03-05 10:00:56 +0530

    Adding map feature

--- controllers/index.js
+++ controllers/index.js
@@ -1,4 +1,8 @@
+const geocoding = require("@mapbox/mapbox-sdk/services/geocoding");
 const Listing = require("../models/listing.js")
+const mbxGeocoding = require('@mapbox/mapbox-sdk/services/geocoding');
+const mapToken  = process.env.MAP_TOKEN;
+const geocodingClient = mbxGeocoding({ accessToken: mapToken})
 
 module.exports.index = async (req,res) => {
     const allListings = await Listing.find({});
@@ -25,13 +29,24 @@ module.exports.showListings = async (req, res) => {
 }
 
 module.exports.createListings = async (req, res, next) => {
+    let response = await geocodingClient
+    .forwardGeocode({
+        query: req.body.listing.location,
+        limit: 1,
+    })
+    .send()
+    console.log(response.body.features[0].geometry)
+    res.send("Done!")
+
     let url = req.file.path
     let filename = req.file.filename
     console.log(url, "..", filename)
     const newListing = new Listing(req.body.listing);
     newListing.owner = req.user._id;
     newListing.image = { url, filename }
-    await newListing.save();
+    newListing.geometry = response.body.features[0].geometry;
+    let savedListing = await newListing.save();
+    console.log(savedListing)
     req.flash("success", "New listings created!")
     res.redirect("/listings");
 }
