185568d616d39cd1eb5cd94c5d1840c79f599e03
SAHIL sanghar <sangharsahil546@gmail.com>
2024-03-06 18:09:49 +0530

    Added coordinate feature to existing maps

--- public/js/map.js
+++ public/js/map.js
@@ -3,12 +3,15 @@
     const map = new mapboxgl.Map({
         container: 'map', // container ID
         style: "mapbox://styles/mapbox/streets-v12",
-        center: [72.58727, 23.02579 ], // starting position [lng, lat]
+        center: listing.geometry.coordinates, // starting position [lng, lat]
         zoom: 9 // starting zoom
     });
 
-    console.log(coordinates)
-    const marker = new mapboxgl.Marker()
-    .setLngLat(coordinates)
+    // console.log(listing.geometry.coordinates)
+
+    const marker = new mapboxgl.Marker({color: "red"})
+    .setLngLat(listing.geometry.coordinates)
+    .setPopup(new mapboxgl.Popup({offset: 25})
+    .setHTML(`<h4>${listing.title}</h4><p>Exact location will be provided after booking</p>`))
     .addTo(map)
     
